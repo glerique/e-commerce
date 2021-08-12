ec1370a48bb2905c46c0f4b806e19da5621e8777
glerique <gael.lerique@gmail.com>
2021-08-12 15:00:38 +0200

    Create StripeService Class

---
+++ src/Service/StripeService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Purchase;
+
+class StripeService
+{
+
+    protected $secretKey;
+    protected $publicKey;
+
+    public function __construct(string $secretKey, string $publicKey)
+    {
+
+        $this->secretKey = $secretKey;
+        $this->publicKey = $publicKey;
+    }
+
+    public function getySecretKey(): string
+    {
+        return $this->secretKey;
+    }
+
+    public function getPublicKey(): string
+    {
+        return $this->publicKey;
+    }
+
+
+    public function getPaymentIntent(Purchase $purchase)
+    {
+        \Stripe\Stripe::setApiKey($this->secretKey);
+
+        return  \Stripe\PaymentIntent::create([
+            'amount' => $purchase->getTotal(),
+            'currency' => 'eur'
+        ]);
+    }
+}
