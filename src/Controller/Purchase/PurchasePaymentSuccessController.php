9db8cd3a6809daca697e02277a2bfd31fd8f3281
glerique <gael.lerique@gmail.com>
2021-09-07 10:02:57 +0200

    Add Event

--- src/Controller/Purchase/PurchasePaymentSuccessController.php
+++ src/Controller/Purchase/PurchasePaymentSuccessController.php
@@ -3,13 +3,10 @@
 namespace App\Controller\Purchase;
 
 use App\Entity\Purchase;
-use App\Service\CartService;
-use App\Repository\PurchaseRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Event\PurchaseSuccessEvent;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-
-
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class PurchasePaymentSuccessController extends PurchaseController
 {
@@ -18,7 +15,7 @@ class PurchasePaymentSuccessController extends PurchaseController
      * @Route("/purchase/done/{id}", name="purchase_payment_success")
      * @IsGranted("ROLE_USER") 
      */
-    public function success($id)
+    public function success($id, EventDispatcherInterface $dispatcher)
     {
 
         $purchase = $this->purchaseRepository->find($id);
@@ -31,6 +28,9 @@ class PurchasePaymentSuccessController extends PurchaseController
         $this->em->flush();
 
         $this->cartService->empty();
+
+        $purchaseEvent = new PurchaseSuccessEvent($purchase);
+        $dispatcher->dispatch($purchaseEvent, 'purchase.success');
 
         $this->addFlash('success', "La commande a été payée et confirmée !");
         return $this->redirectToRoute('purchase_index');
